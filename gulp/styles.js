2823a91580ad0cd60d68acd8009379dd6da68af9
tangniye <niyetang@126.com>
2017-03-31 11:50:19 +0800

    打通

--- gulp/styles.js
+++ gulp/styles.js
@@ -21,28 +21,28 @@ gulp.task('styles', function () {
 });
 
 gulp.task('stylesAuth', function () {
-  return buildSingleScss(path.join(conf.paths.src, '/sass/auth.scss'));
+  return buildSingleScss(path.join(conf.paths.src, '/less/auth.scss'));
 });
 
 gulp.task('styles404', function () {
-  return buildSingleScss(path.join(conf.paths.src, '/sass/404.scss'));
+  return buildSingleScss(path.join(conf.paths.src, '/less/404.scss'));
 });
 
 var buildStyles = function () {
-  var sassOptions = {
+  var lessOptions = {
     style: 'expanded'
   };
 
   var injectFiles = gulp.src([
     path.join(conf.paths.src, '/assets/less/**/_*.less'),
-    '!' + path.join(conf.paths.src, '/sass/theme/conf/**/*.scss'),
-    '!' + path.join(conf.paths.src, '/sass/404.scss'),
-    '!' + path.join(conf.paths.src, '/sass/auth.scss')
+    '!' + path.join(conf.paths.src, '/less/theme/conf/**/*.scss'),
+    '!' + path.join(conf.paths.src, '/less/404.scss'),
+    '!' + path.join(conf.paths.src, '/less/auth.scss')
   ], {read: false});
 
   var injectOptions = {
     transform: function (filePath) {
-      filePath = filePath.replace(conf.paths.src + '/sass/', '');
+      filePath = filePath.replace(conf.paths.src + '/less/', '');
       return '@import "' + filePath + '";';
     },
     starttag: '// injector',
@@ -51,12 +51,12 @@ var buildStyles = function () {
   };
 
   return gulp.src([
-    path.join(conf.paths.src, '/sass/main.scss')
+    path.join(conf.paths.src, '/less/main.less')
   ])
     .pipe($.inject(injectFiles, injectOptions))
     .pipe(wiredep(_.extend({}, conf.wiredep)))
     .pipe($.sourcemaps.init())
-    .pipe($.sass(sassOptions)).on('error', conf.errorHandler('Sass'))
+    .pipe($.less(lessOptions)).on('error', conf.errorHandler('Less'))
     .pipe($.autoprefixer()).on('error', conf.errorHandler('Autoprefixer'))
     .pipe($.sourcemaps.write())
     .pipe(gulp.dest(path.join(conf.paths.tmp, '/serve/app/')));
@@ -64,11 +64,11 @@ var buildStyles = function () {
 
 var buildSingleScss = function (paths) {
 
-  var sassOptions = {
+  var lessOptions = {
     style: 'expanded'
   };
   return gulp.src([paths])
-    .pipe($.sass(sassOptions)).on('error', conf.errorHandler('Sass'))
+    .pipe($.less(lessOptions)).on('error', conf.errorHandler('Less'))
     .pipe($.autoprefixer()).on('error', conf.errorHandler('Autoprefixer'))
     .pipe(gulp.dest(path.join(conf.paths.tmp, '/serve/app/')));
 };
