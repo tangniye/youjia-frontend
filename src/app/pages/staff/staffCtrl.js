683c1f539bcfaccf26e7e6e083867ba94070bf4f
tangniye <niyetang@126.com>
2017-04-08 23:47:50 +0800

    add staff

--- src/app/pages/staff/staffCtrl.js
+++ src/app/pages/staff/staffCtrl.js
@@ -5,7 +5,25 @@
     'use strict';
     function staffCtrl($scope) {
         'ngInject';
-        
+        var vm = $scope;
+        vm.activeIndex = 3;
+        angular.element('.tutors-slider-container-inner').css('width', 1800);
+        var margin = 85,
+            itemWidth = 120,
+            activeWidth = 160;
+        var anchorDom = angular.element('.anchor');
+        console.log(anchorDom)
+
+        vm.seeMore = function (index) {
+            if (index === vm.activeIndex) {
+                return;
+            }
+            var offset = "translateX(" + (index - 3) * 205 + "px)";
+            anchorDom.css('transition', 'none').css('transform', offset).css('transform');
+
+            vm.activeIndex = index;
+        }
+
     }
 
     angular.module('app.pages.staff').controller('staffCtrl', staffCtrl);
