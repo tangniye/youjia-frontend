2e02c8f23fa52a75cb828b413de53275324ce732
tangniye <niyetang@126.com>
2017-04-09 11:01:35 +0800

    add staff service

--- src/app/pages/staff/staffCtrl.js
+++ src/app/pages/staff/staffCtrl.js
@@ -3,26 +3,65 @@
  */
 (function () {
     'use strict';
-    function staffCtrl($scope) {
+    function staffCtrl($scope, Staff, toastr) {
         'ngInject';
         var vm = $scope;
         vm.activeIndex = 3;
-        angular.element('.tutors-slider-container-inner').css('width', 1800);
-        var margin = 85,
-            itemWidth = 120,
-            activeWidth = 160;
+
+        vm.page_total = 1;
+        vm.queryStr = {
+            page: 1,
+            page_size: 5,
+            key: '',
+            show: true
+        };
+
+        getStaffList(vm.queryStr);
+
+        function getStaffList(queryStr) {
+            Staff.getStaffList(queryStr).then(function (res) {
+                vm.staffs = res.items;
+
+                vm.activeIndex = 3;
+                vm.queryStr.page = res.page;
+                vm.page_total = res.page_total;
+
+                getProfile(vm.staffs[2].id);
+            })
+        }
+
+        function getProfile(userid) {
+            Staff.getProfile(userid).then(function (res) {
+                vm.activeStaff = res;
+            })
+        }
+
+        // angular.element('.tutors-slider-container-inner').css('width', 1800);
+        var margin = parseInt(angular.element('.tutor-item').css('marginRight')),
+            itemWidth = angular.element('.avatar-container').outerWidth();
         var anchorDom = angular.element('.anchor');
-        console.log(anchorDom)
 
         vm.seeMore = function (index) {
             if (index === vm.activeIndex) {
                 return;
             }
-            var offset = "translateX(" + (index - 3) * 205 + "px)";
+            var offset = "translateX(" + (index - 3) * (margin + itemWidth) + "px)";
             anchorDom.css('transition', 'none').css('transform', offset).css('transform');
 
             vm.activeIndex = index;
         }
+
+        vm.next = function () {
+            vm.queryStr.page += 1;
+            vm.queryStr.page = vm.queryStr.page >= vm.page_total ? vm.page_total : vm.queryStr.page;
+            getStaffList(vm.queryStr);
+        };
+
+        vm.prev = function () {
+            vm.queryStr.page -= 1;
+            vm.queryStr.page = vm.queryStr.page <= 1 ? 1 : vm.queryStr.page;
+            getStaffList(vm.queryStr);
+        };
 
     }
 
