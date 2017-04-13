08563f8272b411f7836e06707f53091afaccaa8e
mandy <hitangman@qq.com>
2017-04-13 16:41:28 +0800

    1. update lodash 2. add srollto directive in course page

---
+++ src/app/directives/scrollNav.directive.js
@@ -0,0 +1,28 @@
+(function () {
+  function scrollNav() {
+    'ngInject';
+    return {
+      restrict: 'EA',
+      scope: true,
+      link: function ($scope, $elem, $attr) {
+        _.each($elem.find('li'), function (li) {
+          var scrollTo = $(li).attr('scroll-to');
+          if (scrollTo) {
+            $(li).on('click', function () {
+              _.each($elem.find('li'), function (li) {
+                $(li).removeClass('active');
+              });
+              $(this).addClass('active');
+              var height = $('#' + scrollTo).offset().top - 86;
+              $('body').animate({
+                scrollTop: height
+              }, 500)
+            })
+          }
+        })
+      }
+    }
+  }
+
+  angular.module('app').directive('scrollNav', scrollNav);
+})();
