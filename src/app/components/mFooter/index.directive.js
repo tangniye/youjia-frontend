072fb86a2854ccdef868b06072f4dc94a08dfe4f
mandy <hitangman@qq.com>
2017-04-19 14:12:26 +0800

    mobile page footer

---
+++ src/app/components/mFooter/index.directive.js
@@ -0,0 +1,43 @@
+(function () {
+  'use strict';
+
+  /** @ngInject */
+  function mFooter() {
+    return {
+      restrict: 'EA',
+      templateUrl: 'app/components/mFooter/index.html',
+      link: function (scope, element) {
+        var windowWidth = $(window).width();
+        if (windowWidth < 768) {
+          var firstLevelLinks = $('.mfooter__menu > li > a');
+          var secondLevelMenus = $('.mfooter__dropdown-menu');
+          var secondLevelLinks = $('.mfooter__dropdown-menu > li > a');
+
+          _.forEach(firstLevelLinks, function (link) {
+            $(link).on('click', function (e) {
+              if ($(this).hasClass('mfooter__dropdown-toggle')) {
+                var secondMenu = $(this).next();
+                if (! secondMenu.hasClass('show')) {
+                  secondLevelMenus.removeClass('show');
+                }
+                secondMenu.toggleClass('show');
+              } else {
+                secondLevelMenus.removeClass('show');
+              }
+            })
+          });
+
+          _.forEach(secondLevelLinks, function (link) {
+            $(link).on('click', function (e) {
+              secondLevelMenus.removeClass('show');
+            })
+          })
+        }
+      }
+    };
+  }
+
+  angular.module('app.components')
+    .directive('mFooter', mFooter);
+
+})();
