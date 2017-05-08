d6fc67db9b87608ff9fa4ac8776488adcaa8dc30
tangniye <niyetang@126.com>
2017-05-08 15:14:05 +0800

    add wechat inner browser judgment to course controller

--- src/app/pages/course/courseCtrl.js
+++ src/app/pages/course/courseCtrl.js
@@ -1,48 +1,54 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  /** @ngInject */
-  function courseCtrl($scope, Common, $state) {
-    var vm = $scope;
-    vm.title = {};
+    /** @ngInject */
+    function courseCtrl($scope, Common, $state, Browser, Constant) {
+        var vm = $scope;
+        vm.title = {};
 
-    var userAgent = navigator.userAgent.toLowerCase();
+        var qq = Constant.contact.qq;
+        if (Browser.versions.mobile) {
+            var ua = navigator.userAgent.toLowerCase();
+            if (ua.match(/MicroMessenger/i) == "micromessenger") {
+                vm.toggleTip = function () {
+                    angular.element('.wechat-tip').show();
+                };
+            }
+            if (Browser.versions.ios) {
+                vm.qqLink = 'mqq://im/chat?chat_type=wpa&uin=' + qq + '&version=1&src_type=web';
+            }
+            if (Browser.versions.android) {
+                vm.qqLink = 'mqqwpa://im/chat?chat_type=wpa&uin=' + qq;
+            }
+        } else {
+            vm.qqLink = 'tencent://message/?uin=' + qq + '&amp;Site=www.xxx.com&amp;Menu=yes'
+        }
 
-    if (/iphone|ipad|ipod/.test(userAgent)) {
-      vm.qqLink = 'mqq://im/chat?chat_type=wpa&uin=1377716383&version=1&src_type=web';
+        vm.$watch(function () {
+            return $state.current
+        }, function (newVal) {
+            switch (newVal.name) {
+                case 'app.pages.course.ielts':
+                    vm.title.main = '雅思';
+                    vm.title.sub = 'IELTS';
+                    break;
+                case 'app.pages.course.toefl':
+                    vm.title.main = '托福';
+                    vm.title.sub = 'TOEFL';
+                    break;
+                case 'app.pages.course.sat':
+                    vm.title.main = 'SAT';
+                    vm.title.sub = 'SAT';
+                    break;
+            }
+        });
 
-    } else if (/android/.test(userAgent)) {
-      vm.qqLink = 'mqqwpa://im/chat?chat_type=wpa&uin=1377716383';
-    } else {
-      vm.qqLink = 'tencent://message/?uin=1377716383&amp;Site=www.xxx.com&amp;Menu=yes'
+        vm.promptAudition = function () {
+            Common.model.promptModel('auditionModalCtrl', 'app/components/audition-modal/audition-modal.html', 'sm', '', 'login-modal audition-modal')
+        };
+
+
     }
 
-
-    vm.$watch(function () {
-      return $state.current
-    }, function (newVal) {
-      switch (newVal.name) {
-        case 'app.pages.course.ielts':
-          vm.title.main = '雅思';
-          vm.title.sub = 'IELTS';
-          break;
-        case 'app.pages.course.toefl':
-          vm.title.main = '托福';
-          vm.title.sub = 'TOEFL';
-          break;
-        case 'app.pages.course.sat':
-          vm.title.main = 'SAT';
-          vm.title.sub = 'SAT';
-          break;
-      }
-    });
-
-    vm.promptAudition = function () {
-      Common.model.promptModel('auditionModalCtrl', 'app/components/audition-modal/audition-modal.html', 'sm', '', 'login-modal audition-modal')
-    };
-
-
-  }
-
-  angular.module('app.pages.course').controller('courseCtrl', courseCtrl);
+    angular.module('app.pages.course').controller('courseCtrl', courseCtrl);
 })();
