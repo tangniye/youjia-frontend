ae05f0f76ca96a2ac285ba79f988ab8aef3c6a2c
mandy <hitangman@qq.com>
2017-05-10 01:50:57 +0800

    staff-standard section slide in mobile

--- src/app/directives/swipeInMobile.directive.js
+++ src/app/directives/swipeInMobile.directive.js
@@ -5,18 +5,54 @@
   function swipeInMobile($timeout, $interval) {
     return {
       link: function(scope, element, attrs) {
-        console.log('working....')
-        var slides = angular.element.find('[swipe-in-mobile-index]');
-        function swipe () {
-          _.forEach(slides, function(slide, index) {
-            angular.element(slide).animation({
-
-            })
+        var basewidth = 200;
+        var basePadding = 20;
+        var baseStep = basewidth + basePadding;
+        var animateTime = 500;
+        var windowWidth = $(window).width();
+        var maxLeft = windowWidth*0.5 - basewidth*1.5 - basePadding;
+        var minLeft = maxLeft - baseStep * 3;
+        var slides = angular.element.find('.staff-standard-bg');
+        var length = slides.length;
+        var centerIndex = 1;
+        $(element).on('swipeLeft', function () {
+          var left = parseInt($(element).css('left'));
+          if (centerIndex == 4) {
+            $(element).css({
+              left: maxLeft
+            });
+            left = maxLeft;
+            centerIndex = 1;
+          }
+          $(element).animate({
+            left: left - baseStep
+          }, animateTime, function () {
+            centerIndex++;
+          });
+        }).on('swipeRight', function () {
+          var left = parseInt($(element).css('left'));
+          if (centerIndex == 1) {
+            $(element).css({
+              left: minLeft
+            });
+            left = minLeft;
+            centerIndex =4;
+          }
+          $(element).animate({
+            left: left + baseStep
+          }, animateTime, function () {
+            centerIndex--;
+          });
+        });
+        function init() {
+          $(element).css({
+            left: maxLeft
+          });
+          _.forEach(slides, function (slide, index) {
+            $(slide).css({left: index*baseStep})
           });
         }
-        $interval(function () {
-          swipe()
-        }, 3000)
+        init();
       }
     }
   }
