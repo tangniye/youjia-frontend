08286870bddc1d789b1603e104c41d4d70351111
mandy <hitangman@qq.com>
2017-05-10 23:12:24 +0800

    audition validation and style

--- src/app/components/audition-modal/auditionModalCtrl.js
+++ src/app/components/audition-modal/auditionModalCtrl.js
@@ -5,16 +5,19 @@
   function auditionModalCtrl($scope, $uibModalInstance, Audition, toastr) {
     var vm = $scope;
 
-    var regx = /^\d{11}$/;
+    var regx = vm.regx = /^\d{11}$/;
+
 
     vm.is_phone_valid = function () {
       return vm.form.phone && vm.form.phone.$viewValue && regx.test(vm.form.phone.$viewValue)
     };
 
     vm.submit = function (item) {
-      if (vm.form.$valid && vm.is_phone_valid()) {
+      if (vm.form.$valid) {
         Audition.add(item).then(function (res) {
           toastr.success('请等待通知', '预约成功', {'closeButton': true});
+          vm.form.$setPristine();
+          vm.form.$setUntouched();
           $uibModalInstance.close();
         })
       }
